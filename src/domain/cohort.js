8a216c9ea20b770b598f4d0eb88433eb5e4cac08
Max V Olofsson <max.olofsson@se.experis.com>
2024-10-29 16:16:18 +0100

    Added function to getAll and create a cohort

--- src/domain/cohort.js
+++ src/domain/cohort.js
@@ -4,12 +4,34 @@ import dbClient from '../utils/dbClient.js'
  * Create a new Cohort in the database
  * @returns {Cohort}
  */
-export async function createCohort() {
+export async function createCohort({ name, startDate, endDate }) {
+  startDate = new Date(startDate)
+  endDate = new Date(endDate)
   const createdCohort = await dbClient.cohort.create({
-    data: {}
+    data: {
+      name,
+      startDate,
+      endDate,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
   })
 
   return new Cohort(createdCohort.id)
+}
+
+export async function getAllCohorts() {
+  try {
+    const cohorts = await dbClient.cohort.findMany({
+      include: {
+        users: { select: { id: true, role: true } }
+      }
+    })
+    return cohorts
+  } catch (error) {
+    console.error('Error getting cohorts:', error)
+    throw error
+  }
 }
 
 export class Cohort {
