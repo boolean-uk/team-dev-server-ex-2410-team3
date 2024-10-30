55cf8ca3eb62be0b801a505d196fbdf946256826
MMaha1 <mayraam049@gmail.com>
2024-10-30 14:15:26 +0100

    added post.js

---
+++ src/domain/post.js
@@ -0,0 +1,135 @@
+import dbClient from '../utils/dbClient.js'
+
+export default class Post {
+  static fromDb(post) {
+    return new Post(
+      post.id,
+      post.content,
+      post.userId,
+      post.user,
+      post.createdAt,
+      post.updatedAt
+    )
+  }
+
+  static async fromJson(json) {
+    const { content, userId } = json
+
+    return new Post(null, content, userId)
+  }
+
+  constructor(id, content, userId, user = null, createdAt, updatedAt) {
+    this.id = id
+    this.content = content
+    this.userId = userId
+    this.user = user
+    this.createdAt = createdAt
+    this.updatedAt = updatedAt
+  }
+
+  // I've changed the user value at the bottom of this to author in order to match the open api's response.
+  toJSON() {
+    return {
+      post: {
+        id: this.id,
+        content: this.content,
+        createdAt: this.createdAt,
+        updatedAt: this.updatedAt,
+        author: {
+          id: this.user.id,
+          cohortId: this.user.cohortId,
+          role: this.user.role,
+          firstname: this.user.profile?.firstName,
+          lastName: this.user.profile?.lastName,
+          bio: this.user.profile?.bio,
+          githubUrl: this.user.profile?.githubUrl,
+          profileImageUrl: this.user.profile?.profileImageUrl
+        }
+      }
+    }
+  }
+
+  // I've made both the create method and the save method depending on what is preferred.
+  async save() {
+    const data = {
+      content: this.content,
+      userId: this.userId
+    }
+
+    const createdPost = await dbClient.post.create({
+      data,
+      include: {
+        user: true
+      }
+    })
+
+    return Post.fromDb(createdPost)
+  }
+
+  static async create({ content, userId }) {
+    const newPost = await dbClient.post.create({
+      data: {
+        content,
+        userId
+      },
+      include: {
+        user: {
+          include: {
+            profile: true
+          }
+        }
+      }
+    })
+    return newPost
+  }
+
+  static async findById(id) {
+    return Post._findByUnique('id', id)
+  }
+
+  static async findAll() {
+    return Post._findMany()
+  }
+
+  static async findAllByUserId(userId) {
+    return Post._findMany('userId', userId)
+  }
+
+  static async _findByUnique(key, value) {
+    const foundPost = await dbClient.post.findUnique({
+      where: {
+        [key]: value
+      },
+      include: {
+        user: {
+          include: {
+            profile: true
+          }
+        }
+      }
+    })
+
+    return foundPost ? Post.fromDb(foundPost) : null
+  }
+
+  static async _findMany(key, value) {
+    const query = {
+      include: {
+        user: {
+          include: {
+            profile: true
+          }
+        }
+      }
+    }
+
+    if (key !== undefined && value !== undefined) {
+      query.where = {
+        [key]: value
+      }
+    }
+
+    const foundPosts = await dbClient.post.findMany(query)
+    return foundPosts.map((post) => Post.fromDb(post))
+  }
+}
