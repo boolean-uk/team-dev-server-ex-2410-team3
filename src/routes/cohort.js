759144f78cf3f4cb68f2c75d7d19ced87a84ef55
Max V Olofsson <max.olofsson@se.experis.com>
2024-10-30 10:36:48 +0100

    Add CRUD routes for cohorts

--- src/routes/cohort.js
+++ src/routes/cohort.js
@@ -1,5 +1,12 @@
 import { Router } from 'express'
-import { create } from '../controllers/cohort.js'
+import {
+  create,
+  getAll,
+  addUserToCohort,
+  getById,
+  update,
+  deleteCohort
+} from '../controllers/cohort.js'
 import {
   validateAuthentication,
   validateTeacherRole
@@ -7,6 +14,19 @@ import {
 
 const router = Router()
 
+// GET endpoints
+router.get('/', validateAuthentication, getAll)
+router.get('/:cohortId', validateAuthentication, getById)
+
+// POST endpoints
 router.post('/', validateAuthentication, validateTeacherRole, create)
+router.post('/addUser', validateAuthentication, validateTeacherRole, addUserToCohort)
+router.post('/', validateAuthentication, validateTeacherRole, create)
+
+// DELETE endpoints
+router.delete('/:cohortId', validateAuthentication, validateTeacherRole, deleteCohort)
+
+// PUT endpoints
+router.put('/:cohortId', validateAuthentication, validateTeacherRole, update)
 
 export default router
