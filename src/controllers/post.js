cd127553e4e8c5123b4e51ab6b2be26edffc94ab
Felix <felix.mathiasson99@gmail.com>
2024-10-31 10:46:48 +0100

    post now works

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -1,14 +1,48 @@
 import Post from '../domain/post.js'
 import { sendDataResponse, sendMessageResponse } from '../utils/responses.js'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 export const create = async (req, res) => {
   const { content } = req.body
+  const userId = req.user?.id // Ensure req.user is defined
 
-  if (!content) {
-    return sendDataResponse(res, 400, { content: 'Must provide content' })
+  // Validate content length
+  if (!content || content.length < 3) {
+    return sendDataResponse(res, 401, {
+      status: 'fail',
+      message: 'Content has to be more than 3 characters'
+    })
   }
 
-  return sendDataResponse(res, 201, { post: { id: 1, content } })
+  // Ensure userId is defined
+  if (!userId) {
+    return sendDataResponse(res, 401, {
+      status: 'fail',
+      message: 'User not authenticated'
+    })
+  }
+
+  try {
+    // Create a new post in the database
+    const newPost = await prisma.post.create({
+      data: {
+        content,
+        userId
+      }
+    })
+
+    return sendDataResponse(res, 201, {
+      status: 'success',
+      data: {
+        post: newPost
+      }
+    })
+  } catch (err) {
+    console.error('Error creating post:', err)
+    return sendMessageResponse(res, 500, 'Internal Server Error')
+  }
 }
 
 export const getAll = async (req, res) => {
